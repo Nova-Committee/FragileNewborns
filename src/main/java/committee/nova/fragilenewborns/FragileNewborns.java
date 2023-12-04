903504f2288c12e533a9193c25614836685168f9
MikhailTapio <mikhailtapio13468@163.com>
2023-12-04 19:18:54 +0800

    displaytest

---
+++ src/main/java/committee/nova/fragilenewborns/FragileNewborns.java
@@ -0,0 +1,14 @@
+package committee.nova.fragilenewborns;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod("fragilenewborns")
+public class FragileNewborns {
+    public FragileNewborns() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
