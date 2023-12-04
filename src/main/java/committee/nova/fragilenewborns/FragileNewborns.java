5de8fd1e3448156dd04cff99bd8b29958c068123
MikhailTapio <mikhailtapio13468@163.com>
2023-12-04 12:30:05 +0800

    Port to Forge 1.16.5

---
+++ src/main/java/committee/nova/fragilenewborns/FragileNewborns.java
@@ -0,0 +1,15 @@
+package committee.nova.fragilenewborns;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
+
+@Mod("fragilenewborns")
+public class FragileNewborns {
+    public FragileNewborns() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+                () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
