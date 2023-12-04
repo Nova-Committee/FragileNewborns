19e2422a938a7694d366038009b0d943cf1c33e7
MikhailTapio <mikhailtapio13468@163.com>
2023-12-04 19:31:48 +0800

    fix

--- src/main/java/committee/nova/fragilenewborns/mixin/MixinLivingEntity.java
+++ src/main/java/committee/nova/fragilenewborns/mixin/MixinLivingEntity.java
@@ -4,16 +4,46 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity {
+    @Unique
+    private byte fragilenewborns$babyCheckInterval;
+
+    @Unique
+    private boolean fragilenewborns$wasBaby = isBaby();
+
     @Shadow
     public abstract boolean isBaby();
+
+    @Shadow
+    public abstract void setHealth(float p_21154_);
+
+    @Shadow
+    public abstract float getHealth();
 
     @Redirect(method = "getMaxHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D"))
     private double redirect$getMaxHealth(LivingEntity instance, Attribute attr) {
         return isBaby() ? .5 * instance.getAttributeValue(attr) : instance.getAttributeValue(attr);
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void inject$tick(CallbackInfo ci) {
+        if (fragilenewborns$wasBaby && fragilenewborns$check() && !isBaby()) {
+            fragilenewborns$wasBaby = false;
+            setHealth(2 * getHealth());
+        }
+    }
+
+    @Unique
+    private boolean fragilenewborns$check() {
+        fragilenewborns$babyCheckInterval++;
+        fragilenewborns$babyCheckInterval &= 19; // i %= 20
+        return fragilenewborns$babyCheckInterval == 0;
     }
 }
